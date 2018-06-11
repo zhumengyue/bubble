e682caf378b4cbdf47b971c0c6db9371f417c15f
zhumengyue <1165001325@qq.com>
2018-06-11 16:00:22 +0800

    引导页模型+封装axios

---
+++ src/utils/fetch.js
@@ -0,0 +1,45 @@
+/**
+ * Created by WebStorm
+ * User : zhumengyue
+ * Date : 2018/6/11
+ * Time : 14:29
+ * Desc : 封装的axios方法
+ */
+import axios from 'axios'
+// 创建axios实例
+axios.defaults.withCredentials = true; // 允许携带cookie
+
+const service = axios.create({
+  timeout: 5000, // 请求的超时时间
+})
+// 发送请求前处理request的数据
+axios.defaults.transformRequest = [function (data) {
+  let newData = ''
+  for (let k in data) {
+    newData += encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) + '&'
+  }
+  return newData
+}]
+// request拦截器
+service.interceptors.request.use(
+  config => {
+    // 发送请求之前，要做的业务
+    return config
+  },
+  error => {
+    // 错误处理代码
+
+    return Promise.reject(error)
+  }
+)
+// response拦截器
+service.interceptors.response.use(
+  response => {
+    // 数据响应之后，要做的业务
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+export default service
